ba165ba8f99736eff913e9c1f3a905ed0359a4fd
20040654 <20040654@myrp.edu.sg>
2021-05-14 11:22:15 +0800

    Initial commit, with datepicker, timepicker, displaytime completed

---
+++ app/src/main/java/sg/edu/rp/c346/id20040654/demotimeanddateapp/MainActivity.java
@@ -0,0 +1,41 @@
+package sg.edu.rp.c346.id20040654.demotimeanddateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour = tp.getCurrentHour();
+                int minute = tp.getCurrentMinute();
+
+                tvDisplay.setText(String.format("The time is %d:%d.", hour, minute));
+            }
+        });
+    }
+}
